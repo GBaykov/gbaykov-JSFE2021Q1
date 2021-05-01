2e296fee10ce27f0508188cb186b1ac1cba1f2e6
Gleb Baykov <gleb.baykov99@gmail.com>
2021-05-02 01:58:27 +0300

    feat: very bad scroll in Pets-in-zoo(landing)

--- online-zoo/pages/Landing/script.js
+++ online-zoo/pages/Landing/script.js
@@ -1,12 +1,6 @@
 // Switch Theme with checkbox
 const switcher = document.querySelector('input[name="theme-change"]');
-const switchTheme = (e) => {
-    if(e.target.checked) {
-        document.body.classList.add('dark-mode');
-    } else {
-        document.body.classList.remove('dark-mode');
-    }
-}
+
 const body = document.querySelector('.body');
 switcher.addEventListener('click', function(event) {
     if(!event.target.classList.contains('dark-mode')) {
@@ -18,3 +12,41 @@ switcher.addEventListener('click', function(event) {
     }
     
     });
+
+
+
+
+
+
+
+
+    const gap = 30;
+
+const carousel = document.getElementById("carousel"),
+  content = document.getElementById("content"),
+  next = document.getElementById("next"),
+  prev = document.getElementById("prev");
+
+next.addEventListener("click", function(e)  {
+  carousel.scrollBy(width  , 0);
+  if (carousel.scrollWidth !== 0) {
+    prev.style.display = "flex";
+  }
+  if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    //next.style.display = "none";
+    carousel.scrollBy(-(width  )*3, 0);
+  }
+});
+prev.addEventListener("click", function(e) {
+  carousel.scrollBy(-(width  ), 0);
+  if (carousel.scrollLeft - width - gap <= 0) {
+    //prev.style.display = "none";
+    carousel.scrollBy((width  )*3, 0);
+  }
+  if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "flex";
+  }
+});
+
+let width = carousel.offsetWidth;
+window.addEventListener("resize", e => (width = carousel.offsetWidth));
