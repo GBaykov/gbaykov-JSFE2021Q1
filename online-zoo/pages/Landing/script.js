0fb2c46510401a6919d17bc4c5c3238b92b6b22a
Gleb Baykov <gleb.baykov99@gmail.com>
2021-05-02 22:03:11 +0300

    feat: end with Pets corousel(landing)

--- online-zoo/pages/Landing/script.js
+++ online-zoo/pages/Landing/script.js
@@ -20,33 +20,75 @@ switcher.addEventListener('click', function(event) {
 
 
 
-    const gap = 30;
+ //Карусель в блоке Pets in zoo 
 
+    const gap = 30;
 const carousel = document.getElementById("carousel"),
   content = document.getElementById("content"),
   next = document.getElementById("next"),
   prev = document.getElementById("prev");
 
-next.addEventListener("click", function(e)  {
+let n = 0;
+
+function nextSlide() { //изменение блока слайдов и сообщение значения в скролл вперед
   carousel.scrollBy(width  , 0);
-  if (carousel.scrollWidth !== 0) {
-    prev.style.display = "flex";
-  }
-  if (content.scrollWidth - width - gap <= carousel.scrollLeft ) { //+ width
-    
+  n = Math.floor(carousel.scrollLeft /width)+2; //ширина пролистанного на ширину одного шага пролистывания
+  if(n==0) {n=8};
+  if(n==9) {n=1};
+  if(n==10) {n=2};
+  console.log(n);
+elem.value = n;
+    var newValue = elem.value;
+    target.innerHTML =`0${newValue}`;
+  if (content.scrollWidth - width - gap <= carousel.scrollLeft ) { 
     carousel.scrollLeft = 0;
   }
-});
-prev.addEventListener("click", function(e) {
+};
+function prevSlide() {//изменение блока слайдов и сообщение значения в скролл назад
   carousel.scrollBy(-(width  ), 0);
+  n = Math.floor(carousel.scrollLeft /width);//ширина пролистанного на ширину одного шага пролистывания
+  if(n==0) {n=8};
+  if(n==9) {n=1};
+  if(n==10) {n=2};
+  console.log(n);
+  elem.value = n;
+    var newValue = elem.value; 
+    target.innerHTML =`0${newValue}`;
   if (carousel.scrollLeft  - gap <= 0) {//- width
-    
     carousel.scrollLeft = carousel.scrollWidth;
   }
-  if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-    next.style.display = "flex";
-  }
-});
+};
+
+
+next.addEventListener("click", nextSlide);
+
+prev.addEventListener("click", prevSlide);
 
 let width = carousel.offsetWidth;
 window.addEventListener("resize", e => (width )); //width= carousel.offsetWidth было
+
+//изменение значения счестчика в Pets in zoo
+var elem = document.querySelector('input[name="pets-range"]');
+var target = document.querySelector('.pets-number');
+
+var rangeValue = function(){
+  
+  if(+target.innerHTML < elem.value) {
+    var newValue = elem.value;
+    target.innerHTML =`0${newValue}`;
+    carousel.scrollBy((width*(elem.value / +target.innerHTML)), 0);
+  }
+  else if (+target.innerHTML > elem.value) {
+    var newValue = elem.value;
+    target.innerHTML =`0${newValue}`;
+    carousel.scrollBy(-(width*(+target.innerHTML /elem.value)), 0);
+  }
+   //if(newValue<Math.floor(carousel.scrollLeft /width)) {
+    //carousel.scrollBy((-width  ), 0);
+  //}
+};
+
+elem.addEventListener("input", rangeValue);
+elem.addEventListener("change", rangeValue);
+
+
