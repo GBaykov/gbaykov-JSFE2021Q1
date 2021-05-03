806f2604aa4fdce689d31bbf832fde90671babd9
Gleb Baykov <gleb.baykov99@gmail.com>
2021-05-03 15:56:04 +0300

    fix: Landing(change map.img.src in dark theme)

--- online-zoo/pages/Landing/script.js
+++ online-zoo/pages/Landing/script.js
@@ -1,16 +1,22 @@
 // Switch Theme with checkbox
 const switcher = document.querySelector('input[name="theme-change"]');
+const map = document.querySelector('.map-img');
+
 
 const body = document.querySelector('.body');
+
 switcher.addEventListener('click', function(event) {
+  
     if(!event.target.classList.contains('dark-mode')) {
         body.classList.add('dark-mode');
         event.target.classList.add('dark-mode');
         switcher.value = 2;
+        map.src = '../../assets/images/MapWhite.png';
     } else {
         body.classList.remove('dark-mode');
         event.target.classList.remove('dark-mode');
         switcher.value = 1;
+        map.src = '../../assets/images/Map.png';
     }
     });
 
