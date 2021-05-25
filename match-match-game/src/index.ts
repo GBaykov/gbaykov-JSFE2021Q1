38f4473ee2f3b1a8e73080376a0201cd8c1167d7
Gleb Baykov <gleb.baykov99@gmail.com>
2021-05-25 22:14:53 +0300

    feat: make basic html-header

--- match-match-game/src/index.ts
+++ match-match-game/src/index.ts
@@ -3,8 +3,10 @@ import { App } from './app';
 
 window.onload = () => {
   const appElement = document.getElementById('app');
+  const bodyElement = document.getElementById('body');
 
   if(!appElement) throw Error('App root element not found');
+  if(!bodyElement) throw Error('bodyElement not found');
 
   new App(appElement).start();
 }
