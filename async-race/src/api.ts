215309ee34bf45ab9e38a86563d1bd5d6b5ebf65
Gleb Baykov <gleb.baykov99@gmail.com>
2021-06-11 20:17:19 +0300

    fix: delete old unnecessary structure

---
+++ async-race/src/api.ts
@@ -0,0 +1,5 @@
+export const base:string = 'http://localhost:8080/';
+
+export const garage:string = `${base}/garage`;
+export const engine:string = `${base}/engine`;
+export const winners:string = `${base}/winners`;
--- async-race/src/app.ts
+++ async-race/src/app.ts
@@ -1,9 +1,4 @@
-// export class App {
-//   constructor(private readonly rootElement:HTMLElement){
-//   }
-// }   то же самое -- синтаксический сахар
-import { difficulty, gameCards } from './components/game-settings/game-setting';
-import { Game } from './components/game/game';
+
 import { ImageCategoryModel } from './models/image-category-model';
 
 export class App {
@@ -13,7 +8,7 @@ export class App {
 
   constructor(element: HTMLElement) {
     this.rootElement = element;
-    this.game = new Game();
+
     this.rootElement.appendChild(this.game.element);
   }
 
@@ -22,21 +17,7 @@ export class App {
     const categories:ImageCategoryModel[] = await res.json();
     let cat:ImageCategoryModel = categories[0];
     let index = 0;
-    if (difficulty.value === 'easy' && gameCards.value === 'unsorted') {
-      index = 0;
-    }
-    if (difficulty.value === 'hard' && gameCards.value === 'unsorted') {
-      index = 1;
-    }
-    if (difficulty.value === 'easy' && gameCards.value === 'starWars') {
-      index = 2;
-    }
-    if (difficulty.value === 'hard' && gameCards.value === 'starWars') {
-      index = 3;
-    }
-    cat = categories[index];
-    // else cat = categories[3];
-    // cat = categories[1];
+
     const images = cat.images.map((name) => `${cat.category}/${name}`);
     this.game.newGame(images);
   }
